866676833c2a82dfc71f61f4aa38acfb742b6278
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-01-23 21:59:36 -0800

    input time test

--- rbot.js
+++ rbot.js
@@ -299,6 +299,9 @@ controller.hears(["#earthnow"], "direct_message,direct_mention,mention,message_r
 controller.hears(["#timezones (.*)"], "direct_message,direct_mention,mention,message_received,ambient", function (bot, message) {
   bot.reply(message, "baseTime test");
   var inputTime = message.text.match(/#timezones (.*)/i);
+
+  bot.reply(message, "inputTime " + inputTime);
+
   if (inputTime !== undefined) {
     var baseTime = inputTime[1];
   } else {
