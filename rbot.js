99176d82befc2ee5f9a79e6722080c89caee350f
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-01-23 21:33:55 -0800

    timezone update

--- rbot.js
+++ rbot.js
@@ -325,14 +325,43 @@ controller.hears(["#timezones"], "direct_message,direct_mention,mention,message_
  * Display current time for all Reaction team members across the world
  * Los Angeles, Colorado Springs, Connecticut, Lagos, Nairobi, Manila
  */
-controller.hears(["#meeting (.*)"], "direct_message,direct_mention,mention,message_received,ambient", function (bot, message) {
+controller.hears(["#timezones2"], "direct_message,direct_mention,mention,message_received,ambient", function (bot, message) {
+  var americaPacific = moment().tz("America/Los_Angeles").format("hh:mm A") + ":  :flag-us-ca:  Santa Monica";
+  var americaMountain = moment().tz("America/Denver").format("hh:mm A") + ":  :flag-us-co:  Colorado Springs (Spencer)";
+  var americaCentral = moment().tz("America/Chicago").format("hh:mm A") + ":  :flag-us-la:  New Orleans (Nat) / :flag-us-wi:  Sheboygan (Eric D.)";
+  var americaEastern = moment().tz("America/New_York").format("hh:mm A") + ":  :flag-us-ct:  Burlington (Jeremy)";
+  var lagos = moment().tz("Africa/Lagos").format("hh:mm A") + ":  :flag-ng:  Lagos (Seun)";
+  var bahlingen = moment().tz("Europe/Berlin").format("hh:mm A") + ":  :flag-de:  Bahlingen (Michael)";
+  var nairobi = moment().tz("Africa/Nairobi").format("hh:mm A") + ":  :flag-ke:  Nairobi (Joy)";
+  var bengaluru = moment().tz("Asia/Kolkata").format("hh:mm A") + ":  :flag-in:  Bengaluru (Akarshit)";
+  var manila = moment().tz("Asia/Manila").format("hh:mm A") + ":  :flag-ph:  Manila (Brent)";
+
+  bot.reply(message, "Reaction Team Time Zones");
+  bot.reply(message, americaPacific);
+  bot.reply(message, americaMountain);
+  bot.reply(message, americaCentral);
+  bot.reply(message, americaEastern);
+  bot.reply(message, lagos);
+  bot.reply(message, bahlingen);
+  bot.reply(message, nairobi);
+  bot.reply(message, bengaluru);
+  bot.reply(message, manila);
+});
+
+
+/**
+ * #timezones
+ * Display current time for all Reaction team members across the world
+ * Los Angeles, Colorado Springs, Connecticut, Lagos, Nairobi, Manila
+ */
+controller.hears(["#meeting"], "direct_message,direct_mention,mention,message_received,ambient", function (bot, message) {
 //   var newYork    = moment.tz("2014-06-01 12:00", "America/New_York");
 // var losAngeles = newYork.clone().tz("America/Los_Angeles");
 // var london     = newYork.clone().tz("Europe/London");
 //
 //
 //
-bot.reply("hello");
+
 
 
   var inputTime = message.text.match(/#meeting (.*)/i);
@@ -340,7 +369,7 @@ bot.reply("hello");
 
   var baseTime = moment().tz("America/Los_Angeles").format("hh:mm A") + ":  :flag-us-ca:  Santa Monica";
 
-
+bot.reply(message, "hello" + baseTime);
 
   var americaPacific = baseTime.clone().tz("America/Los_Angeles").format("hh:mm A") + ":  :flag-us-ca:  Santa Monica";
   var americaMountain = baseTime.clone().tz("America/Denver").format("hh:mm A") + ":  :flag-us-co:  Colorado Springs (Spencer)";
